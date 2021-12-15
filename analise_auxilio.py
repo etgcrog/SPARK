8a9e8832de3e4383300422e3a388d26b9c5e055e
Eduardo Teles, Helder, Danielle <etgcrog@gmail.com>
2021-12-15 16:11:58 -0300

    docstring

--- analise_auxilio.py
+++ analise_auxilio.py
@@ -1,10 +1,32 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import stddev, avg, max, min, col, expr, count, desc, concat, lit, regexp_replace
 from pyspark.sql.types import FloatType
+from datetime import datetime
+
+"""
+[Dicionário de Dados - Auxílio Emergencial]
+
+MÊS DISPONIBILIZAÇÃO -Ano/Mês a que se refere a parcela, no formato AAAAMM.
+
+UF -Sigla da Unidade Federativa do beneficiário do Auxílio Emergencial.
+CÓDIGO MUNICÍPIO IBGE - Código, no IBGE (Instituto Brasileiro de Geografia e Estatistica), do município do beneficiário do Auxílio Emergencial.
+NOME MUNICÍPIO	Nome do município do beneficiário do Auxílio Emergencial.
+NIS BENEFICIÁRIO - Número de Identificação Social (NIS) do beneficiário do Auxílio Emergencial, caso possua.
+CPF BENEFICIÁRIO - Número no Cadastro de Pessoas Físicas (CPF) do beneficiário do Auxílio Emergencial, caso possua.
+NOME BENEFICIÁRIO - Nome do beneficiário do Auxílio Emergencial.
+NIS RESPONSÁVEL - Número de Identificação Social (NIS) do responsável pelo beneficiário do Auxílio Emergencial, caso possua.
+CPF RESPONSÁVEL - Número no Cadastro de Pessoas Físicas (CPF) do responsável pelo beneficiário do Auxílio Emergencial, caso possua.
+NOME RESPONSÁVEL - Nome do responsável pelo beneficiário do Auxílio Emergencial, caso possua.
+ENQUADRAMENTO - Identifica se o beneficiário é do grupo Bolsa Família, Inscrito no Cadastro Único (CadÚnico) ou Não Inscrito no Cadastro Único (ExtraCad).
+PARCELA - Número sequencial da parcela disponibilizada.
+OBSERVAÇÃO - Indica alterações na parcela disponibilizada como, por exemplo, se foi devolvida ou está retida.
+VALOR BENEFÍCIO - Valor disponibilizado na parcela.
+"""
+i = datetime.now()
 
 spark = SparkSession.builder.master('local').appName('meu-spark').getOrCreate()
 
-#carregando 
+#carregando
 df_junho = spark.read.csv("/media/edudev/_home/auxilio_emegencial_csv/junho.csv",\
     header=True, inferSchema=True, sep=';', encoding='ISO-8859-1')
 df_agosto = spark.read.csv("/media/edudev/_home/auxilio_emegencial_csv/agosto.csv",\
@@ -20,18 +42,21 @@ df_tratado_agosto = df_agosto.select('UF', 'MÊS DISPONIBILIZAÇÃO', 'NOME BENE
 df_tratado_setembro = df_setembro.select('UF', 'MÊS DISPONIBILIZAÇÃO', 'NOME BENEFICIÁRIO', 'NIS RESPONSÁVEL', 'PARCELA', 'VALOR BENEFÍCIO')
 df_tratado_outubro = df_outubro.select('UF', 'MÊS DISPONIBILIZAÇÃO', 'NOME BENEFICIÁRIO', 'NIS RESPONSÁVEL', 'PARCELA', 'VALOR BENEFÍCIO')
 
-#dropando os valores nulos
-# ---------- df_tratado.filter(df_tratado['UF'].isNull()).count() ----- quantidade de nulos
-df_tratado_junho = df_tratado_junho.na.drop()
-df_tratado_agosto = df_tratado_agosto.na.drop()
-df_tratado_setembro = df_tratado_setembro.na.drop()
-df_tratado_outubro = df_tratado_outubro.na.drop()
 # concatenando os dataframes
-# QUANTIDADE DE REGISTROS = 208.527.091  208 MILHOES
-#df = df_tratado_junho.select(concat(col(df_tratado_agosto), col(df_tratado_setembro), col(df_tratado_outubro)))
 temp1 = df_tratado_junho.union(df_tratado_agosto)
 temp2 = temp1.union(df_tratado_setembro)
 df = temp2.union(df_tratado_outubro)
+
+# QUANTIDADE DE REGISTROS = 208.527.091  208 MILHOES
+
+#dropando os valores nulos
+df = df.na.drop()
+# ---------- df_tratado.filter(df_tratado['UF'].isNull()).count() ----- quantidade de nulos
+# df_tratado_junho = df_tratado_junho.na.drop()
+# df_tratado_agosto = df_tratado_agosto.na.drop()
+# df_tratado_setembro = df_tratado_setembro.na.drop()
+# df_tratado_outubro = df_tratado_outubro.na.drop()
+
 #renomeando as colunas
 df = df.select(col("UF").alias("uf") \
 ,col("MÊS DISPONIBILIZAÇÃO").alias("mes") \
@@ -39,20 +64,24 @@ df = df.select(col("UF").alias("uf") \
 ,col("NIS RESPONSÁVEL").alias("nis")\
 ,col("PARCELA").alias("numero_parcela")\
 ,col("VALOR BENEFÍCIO").alias('valor'))
+
 #trocando as virgulas por ponto
 new_df = df.withColumn("valor",  regexp_replace("valor",  ","  ,"."))
 new_df2 = new_df.withColumn("valor", col("valor").cast(FloatType()))
+
 #agrupando por nome
-estatisticas = df.groupBy('nome')\
+estatisticas = new_df2.groupBy('nome')\
 .agg(max("valor").alias('maximo')
 , min("valor").alias('minimo')\
 , avg("valor").alias('media')\
 ,count("valor").alias('quantidade'))
 estatisticas.show(10, truncate = False)
-#filtrando por quantidade de auxilios recebidos
-stats_2 = estatisticas.filter(estatisticas.quantidade >= 2)
-stats_2.orderBy(col('quantidade').desc()).show(truncate=False)
 
+#filtrando por quantidade de auxilios recebidos
+estatisticas.filter(estatisticas['nome'] == "EDUARDO TELES GUIMARAES").show(truncate=False)
+
+f = datetime.now()
+print(f-i)
 
 
 
--- import_spark_context.py
+++ import_spark_context.py
@@ -1,4 +1,7 @@
 from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 
-
-
+spark = SparkSession.builder.master('spark://172.20.0.3:8080')\
+                            .appName('meu-spark')\
+                            .getOrCreate()
+print(spark)
